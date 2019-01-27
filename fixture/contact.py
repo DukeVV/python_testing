e9523b5b3ac1728f9b8fac7bb92020e7d14baeb1
Vakhtang <5022069@gmail.com>
2019-01-27 22:36:19 +0400

    Добавление тестов: удаление первого контакта, изменение первой группы, изменение первого контакта.

--- fixture/contact.py
+++ fixture/contact.py
@@ -7,6 +7,11 @@ class ContactHelper:
     def create_new(self, contact):
         wd = self.app.wd
         wd.find_element_by_link_text("add new").click()
+        self.fill_name(contact, wd)
+        # input data base
+        wd.find_element_by_xpath("//input[21]").click()
+
+    def fill_name(self, contact, wd):
         # fill name
         wd.find_element_by_name("firstname").click()
         wd.find_element_by_name("firstname").clear()
@@ -15,5 +20,18 @@ class ContactHelper:
         wd.find_element_by_name("middlename").send_keys(contact.middlename)
         wd.find_element_by_name("lastname").clear()
         wd.find_element_by_name("lastname").send_keys(contact.lastname)
+
+    def delete_first_contact(self):
+        wd = self.app.wd
+        # select first contact
+        wd.find_element_by_name("selected[]").click()
+        # submit deletein
+        wd.find_element_by_xpath("//input[@value='Delete']").click()
+        wd.switch_to_alert().accept()
+
+    def edit_first_contact(self, contact):
+        wd = self.app.wd
+        wd.find_element_by_xpath("//img[@alt='Edit']").click()
+        self.fill_name(contact, wd)
         # input data base
-        wd.find_element_by_xpath("//input[21]").click()
+        wd.find_element_by_name("update").click()
--- fixture/group.py
+++ fixture/group.py
@@ -9,7 +9,12 @@ class GroupHelper:
         self.open_group_page()
         # init group creation
         wd.find_element_by_name("new").click()
-        # fill group form
+        self.fill_group_form(group, wd)
+        # submit group creation
+        wd.find_element_by_name("submit").click()
+        self.return_to_groups_page()
+
+    def fill_group_form(self, group, wd):
         wd.find_element_by_name("group_name").click()
         wd.find_element_by_name("group_name").clear()
         wd.find_element_by_name("group_name").send_keys(group.name)
@@ -19,9 +24,6 @@ class GroupHelper:
         wd.find_element_by_name("group_footer").click()
         wd.find_element_by_name("group_footer").clear()
         wd.find_element_by_name("group_footer").send_keys(group.footer)
-        # submit group creation
-        wd.find_element_by_name("submit").click()
-        self.return_to_groups_page()
 
     def return_to_groups_page(self):
         wd = self.app.wd
@@ -38,3 +40,14 @@ class GroupHelper:
         wd.find_element_by_name("delete").click()
         self.return_to_groups_page()
 
+    def edit_first_group(self, group):
+        wd = self.app.wd
+        self.open_group_page()
+        # select first group
+        wd.find_element_by_name("selected[]").click()
+        # submit edit group
+        wd.find_element_by_name("edit").click()
+        self.fill_group_form(group, wd)
+        # update edit
+        wd.find_element_by_name("update").click()
+        self.return_to_groups_page()
