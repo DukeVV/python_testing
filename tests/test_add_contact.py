9dab14b18d432b040427e0dd43ab5014cb602bf6
Vakhtang <5022069@gmail.com>
2019-01-30 19:42:30 +0400

    Добавление текущего состояния сесии. Login Logout.

--- tests/test_add_contact.py
+++ tests/test_add_contact.py
@@ -2,7 +2,7 @@
 from model.contact import Contact
 
 
-def test_untitled_test_case(app):
+def test_add_new_contact(app):
     app.contact.create_new(Contact(firstname="Ivan", middlename="Ivanovich", lastname="Ivanov"))
 
 
